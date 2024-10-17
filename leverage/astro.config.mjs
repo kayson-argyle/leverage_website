78bf4fe89f9cb1599430f23371c46637946bf0d2
alecbolingbroke <87875824+alecbolingbroke@users.noreply.github.com>
2024-10-17 16:50:16 +0000

    Fixed build errors with exclusive use of pnpm.

--- leverage/astro.config.mjs
+++ leverage/astro.config.mjs
@@ -4,10 +4,12 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import icon from "astro-icon";
 import react from "@astrojs/react";
+import cloudflare from "@astrojs/cloudflare"
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://astroship.web3templates.com",
   integrations: [tailwind(), mdx(), sitemap(), icon(), react()],
   output: 'hybrid',
+  adapter: cloudflare(),
 });
